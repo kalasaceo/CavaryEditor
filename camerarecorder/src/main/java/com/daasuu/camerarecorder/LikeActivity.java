7196b333bffa85e1822c04793d82ce9bb18bbfd7
kalasaceo <42656065+kalasaceo@users.noreply.github.com>
2021-08-06 15:56:58 +0530

    hfgjgbhjm

---
+++ camerarecorder/src/main/java/com/daasuu/camerarecorder/LikeActivity.java
@@ -0,0 +1,23 @@
+package com.daasuu.camerarecorder;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+public class LikeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_likezero);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                overridePendingTransition(0, 0);
+                finish();
+                overridePendingTransition(0, 0);
+            }
+        }, 1400);
+
+    }
+}
